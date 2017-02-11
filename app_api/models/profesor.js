f6f5adb935f96c7e2088843c049ef38fba861ade
joelerll <joelerll@gmail.com>
2017-02-11 06:30:55 -0500

    Terminado profesor api - faltan Validaciones cedula y satinize - Faltan test

--- app_api/models/profesor.js
+++ app_api/models/profesor.js
@@ -1,19 +1,105 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+bcrypt          = require('bcryptjs'),
+uniqueValidator = require('mongoose-unique-validator'),
+shortId         = require('shortid');
 
-var ProfesoresSchema = mongoose.Schema({
-  correo:              {type: String},
-  clave:               {type: String},
-  rol:                 {type: String},
-  tipo_identificacion: {type: String},
-  identificacion:      {type: String},
-  nombres:             {type: String},
-  apellidos:           {type: String},
-  creado:              {type: Date, default: Date.now},
-  editado:             {type: Date, default: Date.now}
-},{collection: 'profesores', versionKey: false})  //desafios: [{ type : ObjectId, ref: 'Desafio' }]
+var ProfesorSchema = mongoose.Schema({
+  _id: {
+	    type: String,
+	    unique: true,
+	    'default': shortId.generate
+	},
+  correo:              {
+    type: String,
+    validate: {
+      validator: function(v) {
+            return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
+      },
+      message: '{VALUE} no es un correo'
+    },
+    required: [true, 'correo es requerido'],
+    unique: [true]
+  },
+  clave:               {
+    type: String,
+    required: [true, 'clave es requerida']
+  },
+  rol:                 {
+    type: String,
+    enum: ['profesor'],
+    default: 'profesor',
+    required: [true, 'rol es requerido']
+  },
+  tipo_identificacion: {
+    type: String,
+    enum: ['cedula'],
+      default: 'cedula',
+    required: [true, 'tipo identificacion es requerido']
+  },
+  //TODO: regex profesore cedula
+  identificacion:      {
+    type: String,
+    required: [true, 'identificacion es requerida'],
+    unique: true
+  },
+  nombres:             {
+    type: String,
+    required: [true, 'nombres son requeridos']
+  },
+  apellidos:           {
+    type: String,
+    required: [true, 'apellidos son requeridos']
+  }
+},{collection: 'profesores', versionKey: false,timestamps: true})  //desafios: [{ type : ObjectId, ref: 'Desafio' }]
 
-ProfesoresSchema.statics.getProfesores= function(cb) {
-  return this.model('Profesor').find({},cb);
+ProfesorSchema.plugin(uniqueValidator);
+
+ProfesorSchema.pre('save', function (next) {
+  const profesor = this;
+  if (this.isModified('clave') || this.isNew) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(profesor.clave, salt, function(err, hash) {
+        if (err) {
+          return next(err);
+        }
+        profesor.clave = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+ProfesorSchema.methods.create = function(cb) {
+  this.save(cb);
 }
 
-module.exports = mongoose.model('Profesor', ProfesoresSchema)
+ProfesorSchema.statics.update = function(id,admin_nuevo, cb ) {
+   this.model('Profesor').findOneAndUpdate({_id: id}, admin_nuevo, {upsert:true}, cb);
+}
+
+ProfesorSchema.statics.getProfesor = function(correo, cb) {
+  this.model('Profesor').findOne({correo: correo},{clave: 0}, cb);
+}
+
+ProfesorSchema.statics.getProfesorById = function(id, cb) {
+  this.model('Profesor').findOne({_id: id},{clave: 0},cb)
+}
+
+ProfesorSchema.statics.delete = function(id, cb) {
+  this.model('Profesor').findByIdAndRemove(id).exec(cb);
+}
+
+ProfesorSchema.statics.getProfesores = function(cb) {
+  this.model('Profesor').find({},{clave: 0},cb);
+}
+
+ProfesorSchema.statics.getProfesorLike = function(query, cb) {
+  this.model('Profesor').find({$or: [{nombres:  {'$regex': query, '$options': 'i' }}, {apellidos: {'$regex': query, '$options': 'i' }}]}, cb)
+}
+
+module.exports = mongoose.model('Profesor', ProfesorSchema)
