73d9695edd2fa0f967c111485e59e4d5c5d44929
joelerll <joelerll@gmail.com>
2017-02-16 23:56:39 -0500

    panel profesor simple, falta

--- apps_angular/admin/app.module.js
+++ apps_angular/admin/app.module.js
@@ -2,5 +2,6 @@ angular.module('adminApp', [
   'ui.router',
   'ui.bootstrap',
   'profesores',
-  'angular-jwt'
+  'angular-jwt',
+  'angularCSS'
 ]);
--- apps_angular/admin/panel/panel.controller.js
+++ apps_angular/admin/panel/panel.controller.js
@@ -9,4 +9,50 @@ function PanelController (auth,$location) {
     auth.logout();
     $location.path('/');
   }
+
+  vm.tabs = {
+    dashboard: true,
+    estudiantes: false,
+    ayudantes: false,
+    profesores: false,
+    cursos: false
+  }
+
+  vm.tabs.clicks = {
+    dashboard: function () {
+      vm.tabs.dashboard = true;
+      vm.tabs.estudiantes = false;
+      vm.tabs.ayudantes = false;
+      vm.tabs.profesores = false;
+      vm.tabs.cursos = false;
+    },
+    estudiantes: function () {
+      vm.tabs.dashboard = false;
+      vm.tabs.estudiantes = true;
+      vm.tabs.ayudantes = false;
+      vm.tabs.profesores = false;
+      vm.tabs.cursos = false;
+    },
+    ayudantes: function () {
+      vm.tabs.dashboard = false;
+      vm.tabs.estudiantes = false;
+      vm.tabs.ayudantes = true;
+      vm.tabs.profesores = false;
+      vm.tabs.cursos = false;
+    },
+    profesores: function () {
+      vm.tabs.dashboard = false;
+      vm.tabs.estudiantes = false;
+      vm.tabs.ayudantes = false;
+      vm.tabs.profesores = true;
+      vm.tabs.cursos = false;
+    },
+    cursos: function () {
+      vm.tabs.dashboard = false;
+      vm.tabs.estudiantes = false;
+      vm.tabs.ayudantes = false;
+      vm.tabs.profesores = false;
+      vm.tabs.cursos = true;
+    },
+  }
 }
